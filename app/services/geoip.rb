db00804b8508f064744915512f3d5abf877f0de0
rishard <info@masterpro.ws>
2020-11-25 18:32:26 +0300

    adding condition for 'get_country_by_ip' method

--- app/services/geoip.rb
+++ app/services/geoip.rb
@@ -1,7 +1,7 @@
 class Geoip
   def self.remote_ip(ip)
     if ip == '127.0.0.1'
-      '93.185.29.95'
+      '77.111.245.11'
     else
       ip
     end
@@ -10,6 +10,8 @@ class Geoip
   def self.get_country_by_ip(ip)
     url = 'https://www.iplocate.io/api/lookup/' + remote_ip(ip) # + '?apikey=your_api_key'
     response = Net::HTTP.get(URI.parse(url))
-    JSON.parse(response)['country'].gsub(/\s+/, '')
+    if JSON.parse(response)['country'].present?
+    	JSON.parse(response)['country'].gsub(/\s+/, '')
+    end
   end
 end
