c249f0c559fc21b5355453b1dbe99ce60b1877de
rishard <info@masterpro.ws>
2020-11-26 05:17:35 +0300

    fix validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,5 @@ class Post < ApplicationRecord
   #This validates presence of title, and makes sure that the length is not more than 140 words
   validates :title, presence: true, length: {maximum: 140}
   #This validates presence of body
-  validates :body, presence: true
+  #validates :body, presence: true
 end
