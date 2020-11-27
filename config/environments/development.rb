e418b78b41cd969d2dfe1775fc145cf12c0b88b4
rishard <info@masterpro.ws>
2020-11-27 16:31:27 +0300

    adding confirmation email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,17 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Sending emails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['GMAIL_USER_NAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  # Sending email
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => ENV['DOMAIN_NAME'] }
+  # SMTP settings for gmail
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => ENV['GMAIL_USER_NAME'],
+  :password             => ENV['GMAIL_PASSWORD'],
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
