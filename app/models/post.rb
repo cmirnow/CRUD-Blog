8da7aee3dda720a9269084474b722a77c063069f
rishard <info@masterpro.ws>
2020-11-28 20:53:34 +0300

    adding 'dependent: :destroy' to Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  has_many :comments #, dependent: :destroy
+  has_many :comments, dependent: :destroy
   #This validates presence of title, and makes sure that the length is not more than 140 words
   validates :title, presence: true, length: {maximum: 140}
   #This validates presence of body
