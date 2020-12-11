c7e5e25af221ce30ea44bef8d6a6aab71090c3a1
rishard <info@masterpro.ws>
2020-12-11 08:24:23 +0300

    code improvement

--- app/services/get_images.rb
+++ app/services/get_images.rb
@@ -1,8 +1,10 @@
 class GetImages
   def self.get_random_banner(country)
     x = get_country(country)
-    File.join('/images', x, Dir.children(File.join(File.join(Rails.root, 'public', 'images'), x)).sample)
+    File.join('/images', x, Dir.children(File.join(Rails.root, 'public', 'images', x)).sample)
   end
+
+ 
 
   def self.get_country(country)
     if %w[GB US RU NL].include? country
