f393da5feeb2e2a35ecace9e109eeb0e0a0baad2
rishard <info@masterpro.ws>
2020-12-15 01:26:39 +0300

    minor fix

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, length: { minimum: 3 }
-  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create, message: 'Invalid email' }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
   validates :subject, length: { minimum: 5 }
   validates :message, length: { minimum: 15 }
 end
