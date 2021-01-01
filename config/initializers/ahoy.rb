ef05f95993e7e789425f726e8723ffab99f04b67
rishard <info@masterpro.ws>
2021-01-02 01:39:03 +0300

    code improvement

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -9,7 +9,7 @@ Ahoy.user_agent_parser = :device_detector
  # Get the bots and know when they index you...
 Ahoy.track_bots = true 
 
-#Ahoy.server_side_visits = :when_needed
+Ahoy.server_side_visits = :when_needed
 
 # Deleting old entries
 Ahoy::Visit.where("started_at < ?", 1.week.ago).find_in_batches do |visits|
