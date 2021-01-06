1cf79efa01831966b38df26d7e20b33eee2ff76c
rishard <info@masterpro.ws>
2021-01-06 18:17:31 +0300

    code improvement

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -71,19 +71,6 @@ module ApplicationHelper
     end
   end
 
-  def preview(i)
-    if i.images.attached? && !current_page?(root_path)
-      tag.div class: 'preview' do
-        link_to i.images.first, 'data-lightbox' => 'preview' do
-          image_tag i.images.first.variant(
-            combine_options:
-            { resize_to_fill: [400, 300], kuwahara: '3%' }
-          )
-        end
-      end
-    end
-  end
-
   def pagination
       paginate @object if !current_page?(root_path)
   end
--- app/helpers/archive_helper.rb
+++ app/helpers/archive_helper.rb
@@ -1,2 +1,14 @@
 module ArchiveHelper
+  def preview(i)
+    if i.images.attached? && !current_page?(root_path)
+      tag.div class: 'preview' do
+        link_to i.images.first, 'data-lightbox' => 'preview' do
+          image_tag i.images.first.variant(
+            combine_options:
+            { resize_to_fill: [400, 300], kuwahara: '3%' }
+          )
+        end
+      end
+    end
+  end
 end
