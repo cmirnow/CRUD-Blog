6a9bf081d12f9eeb132ace06f6788c8b0191104d
rishard <info@masterpro.ws>
2021-01-16 02:18:57 +0300

    minor fix

--- test/mailers/previews/form_mailer_preview.rb
+++ test/mailers/previews/form_mailer_preview.rb
@@ -2,9 +2,8 @@
 class FormMailerPreview < ActionMailer::Preview
   def new_form_email
     # Set up a temporary order for the preview
-    form = Contact.new(name: "Vasya Pupkin", email: "info@bluhbluhbluh.ru", subject: "niochem", message: "Hello my friend.")
+    form = Contact.new(name: 'Vasya Pupkin', email: 'info@bluhbluhbluh.ru', subject: 'niochem', message: 'Hello my friend.')
 
     FormMailer.with(form: form).new_form_email
   end
 end
-
