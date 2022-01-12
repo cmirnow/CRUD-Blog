e640274bdc92d3142f3486a5fb8b4405ed3eab00
rishard <info@masterpro.ws>
2022-01-12 04:34:44 +0300

    added test

---
+++ spec/requests/posts_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+  describe 'GET index' do
+    it 'has a 200 status code' do
+      get '/'
+      expect(response.status).to eq(200)
+    end
+  end
+end
