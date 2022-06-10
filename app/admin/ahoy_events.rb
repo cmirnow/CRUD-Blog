eff7b18b47995ed88e287a774a7cea2d01818380
rishard <info@masterpro.ws>
2022-06-10 05:44:16 +0300

    the 'edit' link has been hidden from ahoy

--- app/admin/ahoy_events.rb
+++ app/admin/ahoy_events.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Ahoy::Event do
   permit_params :visit_id, :user_id, :name, :properties, :time
   remove_filter :visit, :name
-  actions :all, except: [:new]
+  actions :all, except: [:new, :edit]
 end
